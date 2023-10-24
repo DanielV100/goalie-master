b4e267308e35af8243c39eb5e5913ed3ada8a8b2
DanielV100 <info@d-vollmer.de>
2023-10-24 10:07:03 +0200

    project init

---
+++ src/test/java/dhbw/on/webdev/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dhbw.on.webdev;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
