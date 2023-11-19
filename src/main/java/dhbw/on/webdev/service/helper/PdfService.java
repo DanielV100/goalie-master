1e0479fcea03a8a176d243f596edc4c66da90b13
DanielV100 <info@d-vollmer.de>
2023-11-19 11:36:27 +0100

    cleanedup and refactored pdf service

--- src/main/java/dhbw/on/webdev/service/helper/PdfService.java
+++ src/main/java/dhbw/on/webdev/service/helper/PdfService.java
@@ -1,17 +1,11 @@
 package dhbw.on.webdev.service.helper;
 
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
@@ -20,108 +14,35 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 
+/**
+ * Class contains methods for transforming xml to pdf. Apache-FO? is used therefor.
+ * @see <a href="https://xmlgraphics.apache.org/fop/">Apache FOP</a>
+ * @author daniel
+ */
 @ApplicationScoped
 public class PdfService {
-    private final PDFont headingFont = PDType1Font.HELVETICA_BOLD;
-    private final float headingFontSize = 22;
-    private final PDFont textFont = PDType1Font.HELVETICA;
-    private final float textFontSize = 14;
-    private float lastElementYPosition;
-    private final float marginTop = 50;
-    private final float marginLeft = 80;
-
-    public byte[] createPdf() {
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage(PDRectangle.A4);
-            document.addPage(page);
-
-            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                // Adding a title
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 22);
-                contentStream.newLineAtOffset(100, 700);
-                contentStream.showText("Sample PDF Title");
-                contentStream.endText();
-
-                // Adding some text
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA, 12);
-                contentStream.newLineAtOffset(100, 650);
-                contentStream.showText("This is some sample text in the PDF.");
-                contentStream.endText();
-
-                // Adding some text
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA, 12);
-                contentStream.newLineAtOffset(100, 600);
-                contentStream.showText("This is some sample text in the PDF.");
-                contentStream.endText();
-
-            }
-
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            document.save(out);
-            return out.toByteArray();
-        } catch (Exception e) {
-            throw new RuntimeException("Error creating PDF", e);
-        }
-    }
-
-    //Page Builder
-    private PDPage createInitialPage(final PDDocument document, final String initPageTitle) {
-        PDPage initialPage = new PDPage(PDRectangle.A4);
-        try (PDPageContentStream contentStream = new PDPageContentStream(document, initialPage)) {
-            // Adding a title
-            contentStream.beginText();
-            contentStream.setFont(headingFont, headingFontSize);
-            contentStream.newLineAtOffset(marginLeft, 700);
-            contentStream.showText(initPageTitle);
-            contentStream.endText();
-
-            // Adding some text
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.newLineAtOffset(100, 650);
-            contentStream.showText("This is some sample text in the PDF.");
-            contentStream.endText();
-
-            // Adding some text
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.newLineAtOffset(100, 600);
-            contentStream.showText("This is some sample text in the PDF.");
-            contentStream.endText();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error creating PDF", e);
-        }
-        return initialPage;
-    }
+    /**
+     * Method takes the xml and transforms it with the stylesheet.xsl to a pdf.
+     * @param xmlData, which is wel-formed and the entity as xml
+     * @return byte[] of the pdf
+     * @throws Exception
+     */
     public byte[] convertXmlToPdf(String xmlData) throws Exception {
-        // Configure FOP factory and user agent
+        Log.info("Starting XML to PDF conversion");
+        if (xmlData == null || xmlData.isEmpty()) {
+            throw new IllegalArgumentException("XML data cannot be null or empty.");
+        }
         FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
         FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-
-        // Setup output stream (PDF will be written here)
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        try {
-            // Setup FOP for PDF output
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
-
-            // Load XSLT file
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer(new StreamSource(getClass().getClassLoader().getResourceAsStream("stylesheet.xsl")));
-
-            // Set up input for transformation
             StreamSource src = new StreamSource(new ByteArrayInputStream(xmlData.getBytes()));
-
-            // Perform transformation and FOP processing (XML -> XSL-FO -> PDF)
             transformer.transform(src, new SAXResult(fop.getDefaultHandler()));
-
-        } finally {
-            out.close();
+            return out.toByteArray();
+        } catch (Exception e) {
+            throw new Exception("Error converting XML to PDF", e);
         }
-        return out.toByteArray();
     }
 }
